975e7782d2801922144437753dda51fc0ba54e0c
DuarteDavi <54749367+DuarteDavi@users.noreply.github.com>
2025-04-11 11:38:27 -0300

    foi

--- meu-portifolio/src/App.js
+++ meu-portifolio/src/App.js
@@ -4,6 +4,7 @@ import About from "./components/About";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
+import { FaWhatsapp } from "react-icons/fa"; // 👈 adicionando o ícone
 import "./global.css";
 
 function App() {
@@ -14,6 +15,19 @@ function App() {
         <About />
         <Projects />
         <Contact />
+
+        {/* 👇 Botão do WhatsApp acima do Footer */}
+        <div className="whatsapp-container">
+        <a
+  href="https://wa.me/5553984365541"
+  target="_blank"
+  rel="noopener noreferrer"
+  className="whatsapp-float"
+>
+  <FaWhatsapp size={28} />
+</a>
+        </div>
+
         <Footer />
       </main>
     </div>
--- meu-portifolio/src/components/Footer.js
+++ meu-portifolio/src/components/Footer.js
@@ -1,10 +1,20 @@
 import React from "react";
+import { FaWhatsapp } from "react-icons/fa";
 import "../global.css";
 
 const Footer = () => {
   return (
     <footer className="footer">
       <p>&copy; {new Date().getFullYear()} Meu Portfólio. Todos os direitos reservados.</p>
+
+      <a
+        href="https://wa.me/5553984365541"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="whatsapp-footer"
+      >
+      
+      </a>
     </footer>
   );
 };
